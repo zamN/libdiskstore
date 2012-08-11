b99de9cf641b3bb1ca882098b16dda69f47f0b33
Tom Flanagan <theknio@gmail.com>
2012-08-10 23:00:24 -0700

    refactor stuff to try to show archtechture

--- store.cpp
+++ store.cpp
@@ -6,20 +6,62 @@
 #include "sha2/sha2.h"
 #include "store.h"
 
-Store::Store(std::string path, int file_count) {
-  this->path = path;
-  this->file_count = file_count;
+using namespace std;
+
+File::
+
+File::File(Store& _store) : store(_store) {
+    fname = store.path + string((int)this); // temporary filename for now
+    SHA256_Init(&ctx);
+    file = file(fname, std::ios::out | std::ios::app);
+}
+
+File::close {
+   byte[64] hash;
+   SHA256_End64(&ctx, hash);
+   file.close();
+   // rename fname to final hash (hex)
+   // return hash (bytes)
+}
+
+File::write(byte *data, unint length) {
+  file.write(data, length);
+  SHA256_Update(&ctx, data, length);
+}
+
+
+Store::Store(string _path) : path(_path) {
+
 }
 
 // Creates a new empty block
 // returns: block id
 int Store::open() {
-  std::ostringstream ss;
+
+  // find an unused fd
+  static uint _fd = 0;
+  uint fd = -1;
+  for (int i=0;i<MAX_OPEN_FILES;i++) {
+    if (files[_fd] == NULL) {
+      fd = _fd;
+    }
+    _fd = (_fd + 1) & (MAX_OPEN_FILES-1);
+  }
+
+  if (fd == -1) {
+    // ERROR! MAX_OPEN_FILES are open already
+    return -1;
+  }
+
+  files[fd] = new File();
+
+  return fd;
+
   ss << file_count;
   std::string full_path = path + ss.str();
   char temp[path.length() + 65];
   char hashed_result[65];
-  memcpy(temp, path.c_str(), path.length());
+  memcpy(temp, path.c_str(), path.length());H
 
   // Generates 64 byte hash
   SHA256_CTX ctx;
@@ -35,23 +77,28 @@ int Store::open() {
   std::cout << temp << std::endl;
 
   std::fstream file(temp, std::ios::out | std::ios::app);
-  file_count++;
-  return file_count;
+
+  return fd;
 }
 
 // Writes data to block id
-void Store::write(int id, unsigned char* data) {
+void Store::write(uint fd, unsigned char* data) {
+  // wrap to file
 }
 
 // Finishes writing to the block id.
 // returns: sha256 32 byte hash
-char* Store::close(int id) {
-  return new char[1];
+byte* Store::close(uint fd) {
+  File* file = files[fd];
+  files[fd] = NULL;
+  byte[32] ret = file->close();
+  delete file;
+  return ret;
 }
 
 // Open block sha_value for reading.
 // returns: block id
-int Store::open(std::string sha_value) {
+uint Store::open(std::string hash) {
   return 0;
 }
 
--- store.h
+++ store.h
@@ -2,16 +2,50 @@
 
 #ifndef _STORE_H
 #define _STORE_H
+
+#define MAX_OPEN_FILES (4096)
+
+typedef unsigned char byte;
+typedef unsigned int uint;
+
+class Store;
+
+class File {
+  public:
+    File(Store& store);
+    File(Store& store, const std::string hash);
+    void write(const byte *data, uint length);
+    void read(uint offset, uint length, byte *data);
+    byte* close();
+
+  private:
+    SHA256_CTX hash;
+    fstream file;
+    std::string fname;
+    Store& store;
+};
+
 class Store {
   public:
-    Store(std::string, int);
-    int open();
-    int open(std::string);
-    void write(int, unsigned char*);
-    char* close(int);
-    void read(int, int, int, char*);
+    Store(std::string);
+    // open a file for writing and return the fd
+    uint open();
+
+    // open a file hash for reading and return the fd
+    uint open(const std::string hash);
+
+    // write length bytes into open file fd
+    void write(uint fd, const byte *data, uint length);
+
+    // read length bytes from open file fd, beginning at offset
+    void read(uint fd, uint offset, uint length, byte *data);
+
+    // close file fd, return sha256 of the file's data
+    byte* close(uint fd);
+
   private:
     std::string path;
-    int file_count;
+    File* open_files[MAX_OPEN_FILES];
 };
+
 #endif
